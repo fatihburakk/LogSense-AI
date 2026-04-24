181840bc8eecdbca6ebff715fd511dcb2bc3030c
fatihburakk <fatihburak495@gmail.com>
2026-04-24 17:01:59 +0300

    fixed

--- backend/main.py
+++ backend/main.py
@@ -56,7 +56,13 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://89.252.153.223:30001",
+        "http://89.252.153.223:30000",
+        "http://89.252.153.223",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
